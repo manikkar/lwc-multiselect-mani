eded80fd57b7e5e213541892197fbf5c3a6dbc5f
58031661+undefined@users.noreply.github.com <58031661+undefined@users.noreply.github.com>
2022-09-22 10:22:22 +0000

    Component Completed

--- src/child.js
+++ src/child.js
@@ -1,33 +1,90 @@
-import { LightningElement, api } from "lwc";
-import {options} from './data.js';
+import { LightningElement, api,track } from "lwc";
 export default class Child extends LightningElement {
+    @api selectedOptions = ['2','3'];
+   @track changedOptions = [
+      {label: 'Monday',value: '1',status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},
+      {label: 'Tuesday',value: '2' ,status:'Pending',icon:'standard:task2',variant:'slds-badge slds-theme_warning'},
+      {label: 'Wednesday',value: '3' ,status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},
+      {label: 'Thursday',value: '4',status:'Not Started',icon:'standard:task2',variant:'slds-badge slds-theme_error'},
+      {label: 'Friday',value: '5',status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},
+      {label: 'Saturday',value: '6',status:'Pending',icon:'standard:task2',variant:'slds-badge slds-theme_warning'},
+      {label: 'Sunday',value: '7',status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},  
+   ];  
+ 
+   @track options = [];
+   @track iconName = 'standard:account';
+   @api labelName;
     
-  options = options;
-  selectedList = [];
+    
+    searchInput ='';   
+    isDialogDisplay = false; 
+    isDisplayMessage = false; 
 
-  handleselectAll(event){
-    const toggleList = this.template.querySelectorAll('[data-all-options^="toggle"]');
-      for (const toggleElement of toggleList) {
-          toggleElement.checked = event.target.checked;
+    get globalSelectedItems(){
+      let temp = this.changedOptions.filter(e=> e.ischecked === true);
+      return temp;
+    }
+
+    connectedCallback(){
+        this.changedOptions = this.changedOptions.map(ele =>{
+          return {
+            ...ele,
+            ischecked : this.selectedOptions && this.selectedOptions.indexOf(ele.value)>-1 ? true :false
+          }
+        })
+
+        this.options = this.changedOptions;
+    }  
+    
+
+    handleinput(){       
+          this.isDialogDisplay = true;
+          this.isDisplayMessage = false;
+    }
+
+    onchangeSearchInput(event){
+        this.searchInput = event.target.value;
+        if(this.searchInput.trim().length>0){         
+          this.options = this.changedOptions.filter(ele => ele.label.includes(this.searchInput));
+        }else if(!this.searchInput){
+          this.options = this.changedOptions;
+          this.isDisplayMessage = false;
+          this.isDialogDisplay = true
+        }
+
+        if(this.options.length === 0 ){ 
+            this.isDialogDisplay = false;
+            this.isDisplayMessage = true;
+        }       
+    }
+
+  handleselectAll(event){  
+      for( const opt of this.changedOptions){
+        opt.ischecked = event.target.checked;
       }
-      this.selectedList = event.target.checked ? options : [];
   }
 
   handleOptionselection(event){
-    const toggle = this.template.querySelectorAll('[data-checkall^="checkall"]');
-    let temp = this.selectedList.filter(ele => ele.value === event.currentTarget.dataset.value);
-    
-    if(temp.length === 0){
-      this.selectedList.push({
-          label : event.currentTarget.dataset.label,
-          value : event.currentTarget.dataset.value
-          })
-    }else{
-        this.selectedList = this.selectedList.filter(ele => ele.value !== event.currentTarget.dataset.value);
-    }
+      let temp = this.changedOptions.map(e =>e.value === event.currentTarget.dataset.value
+      ? { ...e, ischecked: event.target.checked }: e );
 
-    toggle[0].checked = this.selectedList.length === this.options.length ? true : false; 
-    
-    console.log('selected list',selectedList);
+      this.changedOptions = temp   
 }
+
+handleCancelClick(){
+   this.isDialogDisplay = false;
+}
+
+handleDoneClick(){      
+    console.log('Final data after',JSON.parse(JSON.stringify(this.globalSelectedItems)));
+    this.isDialogDisplay = false;
+}
+
+handlePillRemoveRecord(event){
+   let temp = this.changedOptions.map(e =>e.value === event.currentTarget.dataset.value
+      ? { ...e, ischecked: false }: e );
+
+      this.changedOptions = temp  
+}
+
 }
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
-const options = [
-  {label: 'Monday',value: '1',status:'Completed',icon:'standard:task',variant:'success'},
-  {label: 'Tuesday',value: '2' ,status:'Pending',icon:'standard:task',variant:'warning'},
-  {label: 'Wednesday',value: '3' ,status:'Completed',icon:'standard:task',variant:'success'},
-  {label: 'Thursday',value: '4',status:'Not Started',icon:'standard:task',variant:'error'},
-  {label: 'Friday',value: '5',status:'Completed',icon:'standard:task',variant:'success'},
-  {label: 'Saturday',value: '6',status:'Completed',icon:'standard:task',variant:'success'},
-  {label: 'Sunday',value: '7',status:'Completed',icon:'standard:task',variant:'success'},  
+const EXoptions = [
+  {label: 'Monday',value: '1',status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},
+  {label: 'Tuesday',value: '2' ,status:'Pending',icon:'standard:task2',variant:'slds-badge slds-theme_warning'},
+  {label: 'Wednesday',value: '3' ,status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},
+  {label: 'Thursday',value: '4',status:'Not Started',icon:'standard:task2',variant:'slds-badge slds-theme_error'},
+  {label: 'Friday',value: '5',status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},
+  {label: 'Saturday',value: '6',},
+  {label: 'Sunday',value: '7',status:'Completed',icon:'standard:task2',variant:'slds-badge slds-theme_success'},  
 ]
 
-export {options}
+export {EXoptions}
